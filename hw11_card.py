2ad6c705c08c780a56510c7ebe2bd37b2e7c7e73
jonorgan <jonorgan26@gmail.com>
2023-11-21 15:19:55 -0500

    Finished question 2 of card

--- hw11_card.py
+++ hw11_card.py
@@ -3,6 +3,8 @@
 ###
 
 import pandas as pd
+from sklearn.naive_bayes import GaussianNB
+from sklearn.model_selection import train_test_split
 
 
 # Question 1 ====================================================================================================
@@ -14,15 +16,42 @@ print("Excel file read...")
 
 def AssignClasses(row):
 	if row['NSP'] == 1:
-		return 1
+		return "1"
 	elif row['NSP'] == 2 or row['NSP'] == 3:
-		return 0
+		return "0"
 	else:
 		return row['NSP']
 
 df['NSP'] = df.apply(AssignClasses, axis=1)
 print("Classes assigned...")
+eodf = len(df.index)
+df = df.drop(index=[0, eodf-3, eodf-2, eodf-1])
 
 print("\n")
 # Question 2 ====================================================================================================
 print("Question 2:")
+
+Y = df[["NSP"]].values
+X = df[["MSTV", "Width", "Mode", "Variance"]].values
+X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size = 0.5, random_state = 0)
+
+NB = GaussianNB()
+NB.fit(X_train, Y_train.ravel())
+predictions_nb = NB.predict(X_test)
+print("1) Naive bayesian class labels predicted...")
+
+print("2) Naive bayesian accuracy: " + str(round(NB.score(X_test, Y_test) * 100, 2)) + "%")
+
+y_actu = pd.Series(Y_test.ravel(), name="Actual")
+y_pred = pd.Series(predictions_nb, name="Predicted")
+cm_nb = pd.crosstab(y_actu, y_pred)
+print("3) Naive bayesian confusion matrix:")
+print(cm_nb)
+
+
+print("\n")
+# Question 3 ====================================================================================================
+print("Question 3:")
+
+
+
